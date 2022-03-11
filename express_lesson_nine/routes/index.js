f60b0453a071c97784a7c2781977bbaa7a74594b
Brad Hall <bhall228@gmail.com>
2022-03-11 00:35:11 -0800

    first commit

---
+++ express_lesson_nine/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var staticModels = require('../staticModels/planets');
+
+router.get('/staticPlanets', function (req, res, next) {
+
+  res.send(JSON.stringify(
+    staticModels.planet
+  ));
+});
+
+router.get('/starTrekPlanets', function(req, res, next){
+  var Planets =[
+    {id: 1, name: "Q'onoS", numberOfMoons: 1},
+    {id: 2, name: "Vulcan", numberOfMoons: 0},
+    {id: 3, name: "Earth", numberOfMoons: 1},
+    {id: 4, name: "Rator III", numberOfMoons: 0}
+  ]
+  res.send(JSON.stringify(Planets));
+})
+
+module.exports = router;
